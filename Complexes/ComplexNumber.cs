a8017f5ce6ce5e348d2f51b37f06ecfb6664a3f3
Loïc <loicx98@gmail.com>
2022-10-22 09:19:51 +0200

    Added project + class with all operators

---
+++ Complexes/ComplexNumber.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Complexes
+{
+    class ComplexNumber
+    {
+        #region Attributes
+        public int realPart;
+        public int imaginaryPart;
+        #endregion
+
+        #region Constructeurs
+        public ComplexNumber(int realPart, int imaginaryPart)
+        {
+            this.realPart = realPart;
+            this.imaginaryPart = imaginaryPart;
+        }
+
+        public ComplexNumber()
+        {
+
+        }
+        #endregion
+
+        #region Méthodes
+
+        public static ComplexNumber Conjugate(ComplexNumber a)
+        {
+            return new ComplexNumber(a.realPart, -(a.imaginaryPart));
+        }
+        public static ComplexNumber operator + (ComplexNumber a, ComplexNumber b)
+        {
+            return new ComplexNumber(a.realPart + b.realPart, a.imaginaryPart + b.imaginaryPart);
+        }
+
+        public static ComplexNumber operator - (ComplexNumber a, ComplexNumber b)
+        {
+            return new ComplexNumber(a.realPart - b.realPart, a.imaginaryPart - b.imaginaryPart);
+        }
+
+        public static ComplexNumber operator * (ComplexNumber a, ComplexNumber b)
+        {
+            return new ComplexNumber(a.realPart * b.realPart - a.imaginaryPart * b.imaginaryPart, a.realPart * b.imaginaryPart + a.imaginaryPart * b.realPart);
+        }
+
+        
+        public static ComplexNumber operator / (ComplexNumber a, ComplexNumber b)
+        {
+            ComplexNumber conjub = Conjugate(b);
+            ComplexNumber tmp = a * conjub;
+            ComplexNumber tmp2 = b * conjub;
+
+            return new ComplexNumber(tmp.realPart / tmp2.realPart, tmp.imaginaryPart / tmp2.realPart);
+        }
+        
+        #endregion
+    }
+}
---
+++ Complexes/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Complexes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int reel1, imag1, reel2, imag2;
+            Console.WriteLine("Real part of complex 1 :");
+            reel1 = (int.Parse(Console.ReadLine()));
+            Console.WriteLine("Imaginary part of complex 1 :");
+            imag1 = (int.Parse(Console.ReadLine()));
+            Console.WriteLine("Real part of complex 2 :");
+            reel2 = (int.Parse(Console.ReadLine()));
+            Console.WriteLine("Imaginary part complex 2 :");
+            imag2 = (int.Parse(Console.ReadLine()));
+
+            ComplexNumber c1 = new ComplexNumber(reel1, imag1);
+            ComplexNumber c2 = new ComplexNumber(reel2, imag2);
+
+            Console.WriteLine("Complex 1: " + c1.realPart + "+" + c1.imaginaryPart + "i");
+            Console.WriteLine("Complex 2: " + c2.realPart + "+" + c2.imaginaryPart + "i");
+
+            ComplexNumber c3 = c1 + c2;
+            ComplexNumber c4 = c1 - c2;
+            ComplexNumber c5 = c1 * c2;
+            ComplexNumber c6 = c1 / c2;
+
+            Console.WriteLine("Complex 3 (c1 + c2): " + c3.realPart + "+" + c3.imaginaryPart + "i");
+            Console.WriteLine("Complex 4 (c1 - c2): " + c4.realPart + "+" + c4.imaginaryPart + "i");
+            Console.WriteLine("Complex 5 (c1 * c2): " + c5.realPart + "+" + c5.imaginaryPart + "i");
+            Console.WriteLine("Complex 6 (c1 / c2): " + c6.realPart + "+" + c6.imaginaryPart + "i");
+
+            Console.ReadLine();
+        }
+    }
+}
